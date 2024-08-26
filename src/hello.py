c02bb7edefa5863af056b79dc85991d745db213f
Abdelrahman Berber <itsabdelrahmanberber@gmail.com>
2024-08-26 03:58:14 +0300

    feat: add Logs to flask

--- src/hello.py
+++ src/hello.py
@@ -1,16 +1,27 @@
-# hello.py
 from flask import Flask, make_response
 from prometheus_flask_exporter import PrometheusMetrics
+import logging
+import sys
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
 
+logger = logging.getLogger('flask-app')
+logger.setLevel(logging.INFO)
+
+handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
 @app.route('/')
 def hello():
+    logger.info('Hello endpoint was accessed')
     return 'Hello, Batot!'
 
 @app.route('/<page_name>')
 def other_page(page_name):
+    logger.warning(f"Attempted to access non-existent page: {page_name}")
     response = make_response('The page named %s does not exist.' \
                              % page_name, 404)
     return response
